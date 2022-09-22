2531005c4b4f0b7104e644b72dde26aaf3117909
Eko Sutrisno <eko@briix.com>
2022-09-22 20:13:39 +0700

    update: change outline button color to solid in property name section.

--- src/components/home/SiwaCliffSection.vue
+++ src/components/home/SiwaCliffSection.vue
@@ -21,8 +21,8 @@
                 </div>
             </div>
             <div>
-                <button type="button" class="rounded-full w-full lg:max-w-max border-2 border-briix-primary transition text-briix-primary hover:bg-briix-blue-1 py-3 px-10 inline-flex items-center justify-center space-x-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <button type="button" class="rounded-full w-full lg:max-w-max transition text-white bg-briix-primary group hover:bg-[#23ade9] py-3 px-10 inline-flex items-center justify-center space-x-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 group-hover:animate-bounce" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                     </svg>
                     <p>Download Brochure</p>
